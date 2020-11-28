3a456b12366bac1fe8933da682a21d5148708029
ARKobelkov <arkobelkov@ya.ru>
2020-11-28 12:00:37 +0300

    доделал футер

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,2 +1,18 @@
 "use strict";
+
+$(function () {
+  /* Поддержка svg спрайтов в IE 11 */
+  svg4everybody(); // Модальное окно - каталог
+
+  $('#catalog-btn').on('click', function (event) {
+    event.preventDefault();
+    $(this).toggleClass('active');
+    $('#modal-catalog').fadeToggle();
+  });
+  $('#catalog-close').on('click', function (event) {
+    event.preventDefault();
+    $('#catalog-btn').removeClass('active');
+    $('#modal-catalog').fadeOut();
+  });
+});
 //# sourceMappingURL=script.js.map
--- src/js/script.js
+++ src/js/script.js
@@ -2,5 +2,17 @@ $(function () {
   /* Поддержка svg спрайтов в IE 11 */
   svg4everybody();
 
-  
+  // Модальное окно - каталог
+  $('#catalog-btn').on('click', function (event) {
+    event.preventDefault();
+    $(this).toggleClass('active');
+    $('#modal-catalog').fadeToggle();
+  });
+
+  $('#catalog-close').on('click', function (event) {
+    event.preventDefault();
+    $('#catalog-btn').removeClass('active');
+    $('#modal-catalog').fadeOut();
+  })
+
 });
