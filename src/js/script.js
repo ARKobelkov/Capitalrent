a7b8daa51765f8350bff203ae814a99c6b61b597
ARKobelkov <arkobelkov@ya.ru>
2020-11-29 14:35:54 +0300

    доделал блок с категориями

--- src/js/script.js
+++ src/js/script.js
@@ -15,4 +15,25 @@ $(function () {
     $('#modal-catalog').fadeOut();
   })
 
+  // Кнопка поиска
+  $('#search-btn').on('click', function (event) {
+    event.preventDefault();
+    $('#search-field').toggleClass('active');
+    $('.categories__btn-icon').toggleClass('hidden');
+  });
+
+  // Кнопка "Открыть все категории"
+  $('#all-categories').on('click', function(event) {
+    event.preventDefault();
+    $('.category__all-categories').toggleClass('active');
+
+    if (!$('.category__all-categories').hasClass('active')) {
+      $('.category__all-categories').html('показать все <br> категории');
+    } else {
+      $('.category__all-categories').html('скрыть <br> категории');
+    };
+    
+    $('.categories__category.hidden').slideToggle();
+  });
+
 });
