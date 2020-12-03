a61ce3406b2f146495344ffd6fdb57da095ae0b7
ARKobelkov <arkobelkov@ya.ru>
2020-12-03 22:44:30 +0300

    доделал второе модальное окно (нужно еще доделать инпуты)

--- src/js/script.js
+++ src/js/script.js
@@ -5,21 +5,70 @@ $(function () {
   // Модальное окно - каталог
   $('#catalog-btn').on('click', function (event) {
     event.preventDefault();
+    if ($('#filter-btn').hasClass('active')) {
+      $('#filter-btn').removeClass('active');
+      $('#modal-filter').fadeOut();
+    }
+    
     $(this).toggleClass('active');
     $('#modal-catalog').fadeToggle();
   });
 
+  // Кнопка закрытия окна - каталог
   $('#catalog-close').on('click', function (event) {
     event.preventDefault();
     $('#catalog-btn').removeClass('active');
     $('#modal-catalog').fadeOut();
   })
 
+  // Модальное окно - фильтр
+  $('#filter-btn').on('click', function (event) {
+    event.preventDefault();
+    if ($('#catalog-btn').hasClass('active')) {
+      $('#catalog-btn').removeClass('active');
+      $('#modal-catalog').fadeOut();
+    }
+  
+    $(this).toggleClass('active');
+    $('#modal-filter').fadeToggle();
+    
+  // Селекты в фильтре
+    $('.modal-filter__select').styler({
+      selectSmartPositioning: false
+    });
+
+    $('.modal-filter__sl-currency').styler({
+      selectSmartPositioning: false
+    })
+
+    $('.modal-filter__sl-strip-input').ionRangeSlider();
+
+  });
+
+  // Кнопка - открыть все фильтры
+  $('#all-filters').on('click', function(event) {
+    event.preventDefault();
+    $(this).toggleClass('active');
+    $('#hiddenFilters').slideToggle();
+  });
+
+   // Кнопка закрытия окна - фильтр
+  $('#filter-close').on('click', function (event) {
+    event.preventDefault();
+    $('#filter-btn').removeClass('active');
+    $('#modal-filter').fadeOut();
+  });
+
   // Кнопка поиска
   $('#search-btn').on('click', function (event) {
     event.preventDefault();
     $('#search-field').toggleClass('active');
     $('.categories__btn-icon').toggleClass('hidden');
+  });
+
+  // Отмена ссылки в категориях
+  $('.category__name').on('click', function (event) {
+    event.preventDefault();
   });
 
   // Кнопка "Открыть все категории"
